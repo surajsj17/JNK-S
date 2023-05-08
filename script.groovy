1bda9fbe2c20c056a9cd782109f18defa22b66a3
surajsj17 <120768713+surajsj17@users.noreply.github.com>
2023-05-08 12:30:28 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,11 +2,11 @@ def Build(){
     sh 'echo "Building......."'
 }
 def Test(){
-    when{
-        expression{
-           params.VERSION == '2'
-        }
-    }
+    //when{
+      //  expression{
+        //   params.VERSION == '2'
+        //}
+    //}
     sh 'echo "Testing........."'
 }
 def Deployed(){
